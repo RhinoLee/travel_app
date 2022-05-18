8dd79d748f4ece7bc2b7295bcd6b76a453541b11
RhinoLee <picklerhino@gmail.com>
2022-05-18 15:15:34 +0800

    docker 環境建置 test

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,8 @@
 const { Pool } = require("pg");
 const dotenv = require('dotenv');
 dotenv.config();
-const connectionString = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
-
 pool = new Pool({
-  connectionString
+  connectionString: process.env.DATABASE_URL
 })
 
 module.exports = pool;
