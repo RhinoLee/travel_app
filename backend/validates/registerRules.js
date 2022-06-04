36deec7b963a13d54035c0f8f94062b1d5d40e14
RhinoLee <picklerhino@gmail.com>
2022-06-05 00:04:41 +0800

    [backend] rules setting

--- backend/validates/registerRules.js
+++ backend/validates/registerRules.js
@@ -16,9 +16,9 @@ const registerRules = [
     });
   })
     .withMessage("驗證錯誤"),
-  body("password")
-    .isLength({ min: 8 })
-    .withMessage("密碼需要至少 8 碼"),
+  // body("password")
+  //   .isLength({ min: 8 })
+  //   .withMessage("密碼需要至少 8 碼"),
   body("repassword").custom((value, { req }) => {
     if (value !== req.body.password) {
       throw new Error("密碼確認不一致")
