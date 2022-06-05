eec9317ccccd71914a63fe89b646eea3af98db97
RhinoLee <picklerhino@gmail.com>
2022-06-05 10:43:45 +0800

    [backend] email HTML, CSS

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,5 +1,8 @@
 const sgMail = require('@sendgrid/mail');
+const mjml2html = require("mjml");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+console.log("mjml2html", mjml2html);
 
 const sendEmail = (mail, token, cate) => new Promise(async (resolve, reject) => {
   let receiveDomain = ""
@@ -7,6 +10,39 @@ const sendEmail = (mail, token, cate) => new Promise(async (resolve, reject) => 
 
   if (cate === "verifyEmail") {
     receiveDomain = process.env.NODE_ENV === "development" ? "http://localhost:3003/verifyEmail/" : "https://travel.rhinoman.com.tw/verifyEmail/"
+
+    // made email html
+    const mjmlToHtml = mjml2html(`
+    <mjml>
+      <mj-body background-color="#fafafa">
+        <mj-section>
+          <mj-column></mj-column>
+        </mj-section>
+        <mj-section border-radius="5px 5px 0 0" padding-bottom="120px" background-url="https://storage.googleapis.com/rhinoman-travel-bucket/email/img_emailbg.png" background-size="cover" background-repeat="no-repeat">
+        </mj-section>
+        
+        <mj-section border-radius="0px 0px 5px 5px" background-color="#ffffff">
+          <mj-column>
+            <mj-text font-size="18px" line-height="12px" letter-spacing="1px" color="#000000">歡迎來到旅行筆記</mj-text>
+            <mj-text font-size="18px" line-height="12px" letter-spacing="1px" color="#000000">請先驗證您的信箱</mj-text>
+            <mj-text letter-spacing="1px" color="#525252">已收到您的註冊資訊，請點擊下方驗證信箱連結，已完成驗證程序，謝謝。</mj-text>
+            <mj-text padding-top="18px"></mj-text>
+            <mj-button href="${receiveDomain}?token=${token}" letter-spacing="3px" background-color="#145049" href="#">驗證信箱</mj-button>
+          </mj-column>
+        </mj-section>
+    
+        <mj-section>
+          <mj-column>
+            <mj-text color="#c55252" padding-top="18px" letter-spacing="3px" align="center">請勿直接回覆本信件</mj-text>
+          </mj-column>
+        </mj-section>
+      </mj-body>
+  </mjml>
+  `);
+
+    console.log("mjmlToHtml", mjmlToHtml);
+
+    // setting email
     msg = {
       to: mail,
       from: {
@@ -14,21 +50,51 @@ const sendEmail = (mail, token, cate) => new Promise(async (resolve, reject) => 
         email: 'picklerhino@gmail.com'
       },
       subject: 'Welcome to travel rhinoman! Confirm Your Email',
-      html: `
-      <h2>歡迎來到旅行筆記!<br/>
-      請先驗證您的信箱</h2>
-  
-      <p>已收到您的註冊資訊，請點擊下方驗證信箱連結，已完成驗證程序，謝謝。</p>
-  
-      <div>
-        驗證信箱連結：<a href="${receiveDomain}?token=${token}">${receiveDomain}?token=${token}</a>
-      </div>
-      `,
+      html: mjmlToHtml.html
+      // html: `
+      // <h2>歡迎來到旅行筆記!<br/>
+      // 請先驗證您的信箱</h2>
+
+      // <p>已收到您的註冊資訊，請點擊下方驗證信箱連結，已完成驗證程序，謝謝。</p>
+
+      // <div>
+      //   驗證信箱連結：<a href="${receiveDomain}?token=${token}">${receiveDomain}?token=${token}</a>
+      // </div>
+      // `,
     };
   }
 
   if (cate === "resetPassword") {
     receiveDomain = process.env.NODE_ENV === "development" ? "http://localhost:3003/resetPassword/" : "https://travel.rhinoman.com.tw/resetPassword/"
+
+    // made email html
+    const mjmlToHtml = mjml2html(`
+    <mjml>
+      <mj-body background-color="#fafafa">
+        <mj-section>
+          <mj-column></mj-column>
+        </mj-section>
+        <mj-section border-radius="5px 5px 0 0" padding-bottom="120px" background-url="https://storage.googleapis.com/rhinoman-travel-bucket/email/img_emailbg.png" background-size="cover" background-repeat="no-repeat">
+        </mj-section>
+        
+        <mj-section border-radius="0px 0px 5px 5px" background-color="#ffffff">
+          <mj-column>
+            <mj-text font-size="18px" line-height="12px" letter-spacing="1px" color="#000000">歡迎來到旅行筆記</mj-text>
+            <mj-text letter-spacing="1px" color="#525252">已收到您的重設密碼要求，請點擊下方重設密碼連結，完成重設密碼程序，謝謝。</mj-text>
+            <mj-text padding-top="18px"></mj-text>
+            <mj-button href="${receiveDomain}?token=${token}" letter-spacing="3px" background-color="#145049" href="#">重設密碼</mj-button>
+          </mj-column>
+        </mj-section>
+
+        <mj-section>
+          <mj-column>
+            <mj-text color="#c55252" padding-top="18px" letter-spacing="3px" align="center">請勿直接回覆本信件</mj-text>
+          </mj-column>
+        </mj-section>
+      </mj-body>
+    </mjml>
+    `);
+
     msg = {
       to: mail,
       from: {
@@ -36,13 +102,14 @@ const sendEmail = (mail, token, cate) => new Promise(async (resolve, reject) => 
         email: 'picklerhino@gmail.com'
       },
       subject: 'Welcome to travel rhinoman! Reset Your Password',
-      html: `
-      <p>已收到您的重設密碼要求，請點擊下方重設密碼連結，已完成重設密碼程序，謝謝。</p>
-  
-      <div>
-        重設密碼連結：<a href="${receiveDomain}?token=${token}">${receiveDomain}?token=${token}</a>
-      </div>
-      `,
+      html: mjmlToHtml.html
+      // html: `
+      // <p>已收到您的重設密碼要求，請點擊下方重設密碼連結，已完成重設密碼程序，謝謝。</p>
+
+      // <div>
+      //   重設密碼連結：<a href="${receiveDomain}?token=${token}">${receiveDomain}?token=${token}</a>
+      // </div>
+      // `,
     };
   }
 
